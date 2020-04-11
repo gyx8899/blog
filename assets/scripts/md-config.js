78853d813a919c45573de1d181a0772a53ed7f8e
Steper Kuo <gyx8899@126.com>
2020-04-11 13:37:21 +0800

    refactor(script): Add md-config script to store md files' info

---
+++ assets/scripts/md-config.js
@@ -0,0 +1,96 @@
+module.exports = {
+    blog: {
+        "daily-life": {
+            "01.个人装换机之Windows系列.md": {
+                "date": "2020年2月9日",
+                "path": "daily-life/01.个人装换机之Windows系列.md"
+            },
+            "02.如何走出个人舒适圈.md": {
+                "date": "2020年2月9日",
+                "path": "daily-life/02.如何走出个人舒适圈.md"
+            },
+            "03.如何做好劳逸结合.md": {
+                "date": "2020年3月9日",
+                "path": "daily-life/03.如何做好劳逸结合.md"
+            },
+            "04.优秀的自我介绍很重要.md": {
+                "date": "2020年3月9日",
+                "path": "daily-life/04.优秀的自我介绍很重要.md"
+            },
+            "05.关于简历.md": {
+                "date": "2020年3月13日",
+                "path": "daily-life/05.关于简历.md"
+            }
+        },
+        "frontend": {
+            "docs": {
+                "Console.md": {
+                    "date": "2020年3月25日",
+                    "path": "frontend/docs/Console.md"
+                },
+                "Dos.md": {
+                    "date": "2020年4月9日",
+                    "path": "frontend/docs/Dos.md"
+                },
+                "Git.md": {
+                    "date": "2020年4月2日",
+                    "path": "frontend/docs/Git.md"
+                },
+                "IntelliJ-IDEA.md": {
+                    "date": "2020年3月16日",
+                    "path": "frontend/docs/IntelliJ-IDEA.md"
+                },
+                "Jest.md": {
+                    "date": "2020年3月22日",
+                    "path": "frontend/docs/Jest.md"
+                },
+                "NPM.md": {
+                    "date": "2020年2月23日",
+                    "path": "frontend/docs/NPM.md"
+                },
+                "ReactHook.md": {
+                    "date": "2020年2月18日",
+                    "path": "frontend/docs/ReactHook.md"
+                },
+                "Snippet.md": {
+                    "date": "2020年2月18日",
+                    "path": "frontend/docs/Snippet.md"
+                },
+                "Tricks.md": {
+                    "date": "2020年2月18日",
+                    "path": "frontend/docs/Tricks.md"
+                },
+                "Vue.md": {
+                    "date": "2020年2月18日",
+                    "path": "frontend/docs/Vue.md"
+                },
+                "Testing.md": {
+                    "date": "2020年3月13日",
+                    "path": "frontend/docs/Testing.md"
+                },
+                "WebAPI.md": {
+                    "date": "2020年2月18日",
+                    "path": "frontend/docs/WebAPI.md"
+                },
+                "Webpack.md": {
+                    "date": "2020年2月18日",
+                    "path": "frontend/docs/Webpack.md"
+                }
+            },
+            "react": {
+                "React-生命周期方法快速转 Hooks（1）.md": {
+                    "date": "2020年2月9日",
+                    "path": "frontend/react/React-生命周期方法快速转 Hooks（1）.md"
+                },
+                "React-生命周期方法快速转 Hooks（2）.md": {
+                    "date": "2020年2月9日",
+                    "path": "frontend/react/React-生命周期方法快速转 Hooks（2）.md"
+                }
+            }
+        },
+        "README.md": {
+            "date": "2020年4月11日",
+            "path": "README.md"
+        }
+    }
+};
