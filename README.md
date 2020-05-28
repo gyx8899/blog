edf345b484788784e29f6804b503ba2123fa7c62
Steper Kuo <gyx8899@126.com>
2020-05-28 23:16:15 +0800

    docs(InstallSystem-windows.md): updated;

--- README.md
+++ README.md
@@ -10,11 +10,11 @@
 ## Daily-life
 日常工作中的随感
 
-- [AboutResume](/daily-life/AboutResume.md): <sub><sup>(2020年3月13日)</sup></sub>
-- [CombineExertionAndRest](/daily-life/CombineExertionAndRest.md): <sub><sup>(2020年3月9日)</sup></sub>
-- [GetOutOfYourComfortZone](/daily-life/GetOutOfYourComfortZone.md): <sub><sup>(2020年2月9日)</sup></sub>
-- [InstallSystem-windows](/daily-life/InstallSystem-windows.md): <sub><sup>(2020年2月9日)</sup></sub>
-- [TheImportantOfSelfIntroduction](/daily-life/TheImportantOfSelfIntroduction.md): <sub><sup>(2020年3月9日)</sup></sub>
+- [关于简历](/daily-life/AboutResume.md): <sub><sup>(2020年3月13日)</sup></sub>
+- [如何做好劳逸结合](/daily-life/CombineExertionAndRest.md): <sub><sup>(2020年3月9日)</sup></sub>
+- [如何走出个人舒适圈](/daily-life/GetOutOfYourComfortZone.md): <sub><sup>(2020年2月9日)</sup></sub>
+- [个人装换机之 Windows 系列](/daily-life/InstallSystem-windows.md): <sub><sup>(2020年5月28日)</sup></sub>
+- [优秀的自我介绍很重要](/daily-life/TheImportantOfSelfIntroduction.md): <sub><sup>(2020年3月9日)</sup></sub>
 
 ## Frontend
 前端知识总结
@@ -23,37 +23,37 @@
 ### Docs
 个人知识文档
 
-- [Bash](/frontend/docs/Bash.md): <sub><sup>(2020年5月10日)</sup></sub>
-- [CSS](/frontend/docs/CSS.md): <sub><sup>(2020年5月15日)</sup></sub>
-- [Console](/frontend/docs/Console.md): <sub><sup>(2020年3月25日)</sup></sub>
+- [Bash: 入门重点](/frontend/docs/Bash.md): <sub><sup>(2020年5月10日)</sup></sub>
+- [CSS: 日常拾遗](/frontend/docs/CSS.md): <sub><sup>(2020年5月15日)</sup></sub>
+- [Console: 不用不知道](/frontend/docs/Console.md): <sub><sup>(2020年3月25日)</sup></sub>
 - [DOS command](/frontend/docs/Dos.md): <sub><sup>(2020年5月27日)</sup></sub>
-- [Git](/frontend/docs/Git.md): <sub><sup>(2020年5月13日)</sup></sub>
-- [IntelliJ-IDEA](/frontend/docs/IntelliJ-IDEA.md): <sub><sup>(2020年3月16日)</sup></sub>
-- [Jest](/frontend/docs/Jest.md): <sub><sup>(2020年3月22日)</sup></sub>
-- [NPM](/frontend/docs/NPM.md): <sub><sup>(2020年4月30日)</sup></sub>
-- [React](/frontend/docs/React.md): <sub><sup>(2020年5月9日)</sup></sub>
-- [ReactHook](/frontend/docs/ReactHook.md): <sub><sup>(2020年2月18日)</sup></sub>
-- [Regexp](/frontend/docs/Regexp.md): <sub><sup>(2020年5月9日)</sup></sub>
-- [SEO](/frontend/docs/SEO.md): <sub><sup>(2020年4月17日)</sup></sub>
-- [Snippet](/frontend/docs/Snippet.md): <sub><sup>(2020年2月18日)</sup></sub>
-- [Testing](/frontend/docs/Testing.md): <sub><sup>(2020年3月13日)</sup></sub>
-- [Tricks](/frontend/docs/Tricks.md): <sub><sup>(2020年2月18日)</sup></sub>
-- [Vue](/frontend/docs/Vue.md): <sub><sup>(2020年2月18日)</sup></sub>
-- [WebAPI](/frontend/docs/WebAPI.md): <sub><sup>(2020年2月18日)</sup></sub>
-- [Webpack](/frontend/docs/Webpack.md): <sub><sup>(2020年4月28日)</sup></sub>
-- [Windows](/frontend/docs/Windows.md): <sub><sup>(2020年5月12日)</sup></sub>
+- [Git: 个人搜集](/frontend/docs/Git.md): <sub><sup>(2020年5月13日)</sup></sub>
+- [IntelliJ-IDEA: 使用技巧](/frontend/docs/IntelliJ-IDEA.md): <sub><sup>(2020年3月16日)</sup></sub>
+- [Jest: 测试是为了更好的开发](/frontend/docs/Jest.md): <sub><sup>(2020年3月22日)</sup></sub>
+- [NPM: 点滴记录](/frontend/docs/NPM.md): <sub><sup>(2020年4月30日)</sup></sub>
+- [React: 总结整理](/frontend/docs/React.md): <sub><sup>(2020年5月9日)</sup></sub>
+- [ReactHooks: 学以致用](/frontend/docs/ReactHook.md): <sub><sup>(2020年2月18日)</sup></sub>
+- [Regexp: 日常表达式](/frontend/docs/Regexp.md): <sub><sup>(2020年5月9日)</sup></sub>
+- [SEO: 搜索优化学习](/frontend/docs/SEO.md): <sub><sup>(2020年4月17日)</sup></sub>
+- [Snippet: 那些有趣的代码片段](/frontend/docs/Snippet.md): <sub><sup>(2020年2月18日)</sup></sub>
+- [Testing: 测试技巧笔记](/frontend/docs/Testing.md): <sub><sup>(2020年3月13日)</sup></sub>
+- [Tricks: 技巧黑暗料理](/frontend/docs/Tricks.md): <sub><sup>(2020年2月18日)</sup></sub>
+- [Vue: 点滴都是赞](/frontend/docs/Vue.md): <sub><sup>(2020年2月18日)</sup></sub>
+- [WebAPI: 那些容易忘，容易漏的必回方法](/frontend/docs/WebAPI.md): <sub><sup>(2020年2月18日)</sup></sub>
+- [Webpack: 常规笔记](/frontend/docs/Webpack.md): <sub><sup>(2020年4月28日)</sup></sub>
+- [Windows: 小打小闹](/frontend/docs/Windows.md): <sub><sup>(2020年5月12日)</sup></sub>
 
 ### React
 React 相关知识总结
 
-- [React-生命周期方法快速转 Hooks（1）](/frontend/react/React-%E7%94%9F%E5%91%BD%E5%91%A8%E6%9C%9F%E6%96%B9%E6%B3%95%E5%BF%AB%E9%80%9F%E8%BD%AC%20Hooks%EF%BC%881%EF%BC%89.md): <sub><sup>(2020年2月9日)</sup></sub>
-- [React-生命周期方法快速转 Hooks（2）](/frontend/react/React-%E7%94%9F%E5%91%BD%E5%91%A8%E6%9C%9F%E6%96%B9%E6%B3%95%E5%BF%AB%E9%80%9F%E8%BD%AC%20Hooks%EF%BC%882%EF%BC%89.md): <sub><sup>(2020年2月9日)</sup></sub>
-- [Redux](/frontend/react/Redux.md): <sub><sup>(2020年4月22日)</sup></sub>
-- [Router](/frontend/react/Router.md): <sub><sup>(2020年4月22日)</sup></sub>
+- [React: 生命周期方法快速转 Hooks（1）](/frontend/react/React-%E7%94%9F%E5%91%BD%E5%91%A8%E6%9C%9F%E6%96%B9%E6%B3%95%E5%BF%AB%E9%80%9F%E8%BD%AC%20Hooks%EF%BC%881%EF%BC%89.md): <sub><sup>(2020年2月9日)</sup></sub>
+- [React: 生命周期方法快速转 Hooks（2）](/frontend/react/React-%E7%94%9F%E5%91%BD%E5%91%A8%E6%9C%9F%E6%96%B9%E6%B3%95%E5%BF%AB%E9%80%9F%E8%BD%AC%20Hooks%EF%BC%882%EF%BC%89.md): <sub><sup>(2020年2月9日)</sup></sub>
+- [Redux: 精髓](/frontend/react/Redux.md): <sub><sup>(2020年4月22日)</sup></sub>
+- [Router: 特殊之处](/frontend/react/Router.md): <sub><sup>(2020年4月22日)</sup></sub>
 
 ## Office
 Office 工具技巧
 
 - [Word: 文档操作精选技巧](/office/Word.md): <sub><sup>(2020年5月27日)</sup></sub>
 
-最后更新于2020年5月27日
+最后更新于2020年5月28日
--- assets/scripts/md-config.js
+++ assets/scripts/md-config.js
@@ -3,30 +3,36 @@ module.exports = {
         "daily-life": {
             "AboutResume.md": {
                 "date": "2020年3月13日",
-                "path": "daily-life/AboutResume.md"
+                "path": "daily-life/AboutResume.md",
+                "title": "关于简历"
             },
             "CombineExertionAndRest.md": {
                 "date": "2020年3月9日",
-                "path": "daily-life/CombineExertionAndRest.md"
+                "path": "daily-life/CombineExertionAndRest.md",
+                "title": "如何做好劳逸结合"
             },
             "GetOutOfYourComfortZone.md": {
                 "date": "2020年2月9日",
-                "path": "daily-life/GetOutOfYourComfortZone.md"
+                "path": "daily-life/GetOutOfYourComfortZone.md",
+                "title": "如何走出个人舒适圈"
             },
             "InstallSystem-windows.md": {
-                "date": "2020年2月9日",
-                "path": "daily-life/InstallSystem-windows.md"
+                "date": "2020年5月28日",
+                "path": "daily-life/InstallSystem-windows.md",
+                "title": "个人装换机之 Windows 系列"
             },
             "TheImportantOfSelfIntroduction.md": {
                 "date": "2020年3月9日",
-                "path": "daily-life/TheImportantOfSelfIntroduction.md"
+                "path": "daily-life/TheImportantOfSelfIntroduction.md",
+                "title": "优秀的自我介绍很重要"
             }
         },
         "frontend": {
             "docs": {
                 "Console.md": {
                     "date": "2020年3月25日",
-                    "path": "frontend/docs/Console.md"
+                    "path": "frontend/docs/Console.md",
+                    "title": "Console: 不用不知道"
                 },
                 "Dos.md": {
                     "date": "2020年5月27日",
@@ -35,89 +41,110 @@ module.exports = {
                 },
                 "Git.md": {
                     "date": "2020年5月13日",
-                    "path": "frontend/docs/Git.md"
+                    "path": "frontend/docs/Git.md",
+                    "title": "Git: 个人搜集"
                 },
                 "IntelliJ-IDEA.md": {
                     "date": "2020年3月16日",
-                    "path": "frontend/docs/IntelliJ-IDEA.md"
+                    "path": "frontend/docs/IntelliJ-IDEA.md",
+                    "title": "IntelliJ-IDEA: 使用技巧"
                 },
                 "Jest.md": {
                     "date": "2020年3月22日",
-                    "path": "frontend/docs/Jest.md"
+                    "path": "frontend/docs/Jest.md",
+                    "title": "Jest: 测试是为了更好的开发"
                 },
                 "NPM.md": {
                     "date": "2020年4月30日",
-                    "path": "frontend/docs/NPM.md"
+                    "path": "frontend/docs/NPM.md",
+                    "title": "NPM: 点滴记录"
                 },
                 "ReactHook.md": {
                     "date": "2020年2月18日",
-                    "path": "frontend/docs/ReactHook.md"
+                    "path": "frontend/docs/ReactHook.md",
+                    "title": "ReactHooks: 学以致用"
                 },
                 "Snippet.md": {
                     "date": "2020年2月18日",
-                    "path": "frontend/docs/Snippet.md"
+                    "path": "frontend/docs/Snippet.md",
+                    "title": "Snippet: 那些有趣的代码片段"
                 },
                 "Tricks.md": {
                     "date": "2020年2月18日",
-                    "path": "frontend/docs/Tricks.md"
+                    "path": "frontend/docs/Tricks.md",
+                    "title": "Tricks: 技巧黑暗料理"
                 },
                 "Vue.md": {
                     "date": "2020年2月18日",
-                    "path": "frontend/docs/Vue.md"
+                    "path": "frontend/docs/Vue.md",
+                    "title": "Vue: 点滴都是赞"
                 },
                 "Testing.md": {
                     "date": "2020年3月13日",
-                    "path": "frontend/docs/Testing.md"
+                    "path": "frontend/docs/Testing.md",
+                    "title": "Testing: 测试技巧笔记"
                 },
                 "WebAPI.md": {
                     "date": "2020年2月18日",
-                    "path": "frontend/docs/WebAPI.md"
+                    "path": "frontend/docs/WebAPI.md",
+                    "title": "WebAPI: 那些容易忘，容易漏的必回方法"
                 },
                 "Webpack.md": {
                     "date": "2020年4月28日",
-                    "path": "frontend/docs/Webpack.md"
+                    "path": "frontend/docs/Webpack.md",
+                    "title": "Webpack: 常规笔记"
                 },
                 "SEO.md": {
                     "date": "2020年4月17日",
-                    "path": "frontend/docs/SEO.md"
+                    "path": "frontend/docs/SEO.md",
+                    "title": "SEO: 搜索优化学习"
                 },
                 "React.md": {
                     "date": "2020年5月9日",
-                    "path": "frontend/docs/React.md"
+                    "path": "frontend/docs/React.md",
+                    "title": "React: 总结整理"
                 },
                 "Bash.md": {
                     "date": "2020年5月10日",
-                    "path": "frontend/docs/Bash.md"
+                    "path": "frontend/docs/Bash.md",
+                    "title": "Bash: 入门重点"
                 },
                 "Regexp.md": {
                     "date": "2020年5月9日",
-                    "path": "frontend/docs/Regexp.md"
+                    "path": "frontend/docs/Regexp.md",
+                    "title": "Regexp: 日常表达式"
                 },
                 "Windows.md": {
                     "date": "2020年5月12日",
-                    "path": "frontend/docs/Windows.md"
+                    "path": "frontend/docs/Windows.md",
+                    "title": "Windows: 小打小闹"
                 },
                 "CSS.md": {
                     "date": "2020年5月15日",
-                    "path": "frontend/docs/CSS.md"
+                    "path": "frontend/docs/CSS.md",
+                    "title": "CSS: 日常拾遗"
                 }
             },
             "react": {
                 "React-生命周期方法快速转 Hooks（1）.md": {
                     "date": "2020年2月9日",
-                    "path": "frontend/react/React-生命周期方法快速转 Hooks（1）.md"
+                    "path": "frontend/react/React-生命周期方法快速转 Hooks（1）.md",
+                    "title": "React: 生命周期方法快速转 Hooks（1）"
                 },
                 "React-生命周期方法快速转 Hooks（2）.md": {
                     "date": "2020年2月9日",
-                    "path": "frontend/react/React-生命周期方法快速转 Hooks（2）.md"
+                    "path": "frontend/react/React-生命周期方法快速转 Hooks（2）.md",
+                    "title": "React: 生命周期方法快速转 Hooks（2）"
                 },
                 "Router.md": {
                     "date": "2020年4月22日",
-                    "path": "frontend/react/Router.md"
+                    "path": "frontend/react/Router.md",
+                    "title": "Router: 特殊之处"
                 },
                 "Redux.md": {
                     "date": "2020年4月22日",
-                    "path": "frontend/react/Redux.md"
+                    "path": "frontend/react/Redux.md",
+                    "title": "Redux: 精髓"
                 }
             }
         },
